a0ebc96264a2de5f71bc556357d2e3d50dc77f02
Wyatt Greenway <th317erd@gmail.com>
2019-01-10 17:45:46 -0700

    Updates

--- mass-helper-tool.js
+++ mass-helper-tool.js
@@ -11,39 +11,80 @@ function walkFiles(walkPath, cb, _opts) {
   for (var i = 0, il = files.length; i < il; i++) {
     var fileName = files[i],
         fullFileName = PATH.join(walkPath, fileName),
-        stat = FS.lstatSync(fullFileName);
+        stat = FS.lstatSync(fullFileName),
+        isDirectory = stat.isDirectory(),
+        args = { fullFileName, fileName, stat, walkFiles, isDirectory, path: walkPath };
 
-    if (typeof filter === 'function' && !filter(fullFileName, fileName, stat, walkFiles))
+    if (typeof filter === 'function' && !filter(args))
       continue;
 
-    if (stat.isDirectory())
-      walkFiles(fullFileName, cb, opts);
-    else if (stat.isFile())
-      cb(fullFileName, fileName, walkPath);
+    if (isDirectory) {
+      if (opts.recurse !== false)
+        walkFiles(fullFileName, cb, opts);
+    } else if (!stat.isFile()) {
+      continue;
+    }
+
+    cb(args);
   }
 }
 
-walkFiles(PATH.join(__dirname, 'packages'), (fullFileName, fileName) => {
-  var packageName;
-  fullFileName.replace(/\/packages\/([^\/]+)\//, (m, p) => {
-    packageName = p;
+function updateAllPackageJSONs() {
+  walkFiles(PATH.join(__dirname, 'packages'), ({ fullFileName, isDirectory }) => {
+    if (isDirectory)
+      return;
+
+    var packageName;
+    fullFileName.replace(/\/packages\/([^\/]+)\//, (m, p) => {
+      packageName = p;
+    });
+
+    var json = require(fullFileName);
+    json.repository = `https://github.com/eVisit/react-ameliorate/tree/master/packages/${packageName}`;
+    json.name = `@react-ameliorate/${packageName}`;
+    json.main = packageName.replace(/^(react-ameliorate-component-|react-ameliorate-)/, '') + '.js';
+    json.homepage = `https://github.com/eVisit/react-ameliorate/tree/master/packages/${packageName}`;
+
+    //console.log({ repo: json.repository, name: json.name, main: json.main, homepage: json.homepage });
+
+    FS.writeFileSync(fullFileName, JSON.stringify(json, undefined, 2));
+  }, {
+    filter: ({ fileName, stat }) => {
+      if (stat.isDirectory())
+        return (!fileName.match(/^(react-ameliorate-react-native-shims)$/));
+
+      return (fileName === 'package.json');
+    }
+  });
+}
+
+function copySupportFilesToPackages() {
+  var files = [
+    '.eslintrc',
+    '.npmignore',
+    'LICENSE'
+  ].map((name) => {
+    var fullFileName = PATH.join(__dirname, name);
+
+    return {
+      name,
+      fullFileName,
+      contents: FS.readFileSync(fullFileName)
+    };
   });
 
-  var json = require(fullFileName);
-  json.repository = `https://github.com/eVisit/react-ameliorate/tree/master/packages/${packageName}`;
-  json.name = `@react-ameliorate/${packageName}`;
-  json.main = packageName.replace(/^(react-ameliorate-component-|react-ameliorate-)/, '') + '.js';
-  json.homepage = `https://github.com/eVisit/react-ameliorate/tree/master/packages/${packageName}`;
+  walkFiles(PATH.join(__dirname, 'packages'), ({ fileName, isDirectory, path }) => {
+    if (!isDirectory)
+      return;
 
-  //console.log({ repo: json.repository, name: json.name, main: json.main, homepage: json.homepage });
+    files.forEach((thisFile) => {
+      var writePath = PATH.join(path, fileName, thisFile.name);
+      console.log('Write: ', thisFile.name, ' -> ', writePath);
+      FS.writeFileSync(writePath, thisFile.contents);
+    });
+  }, {
+    recurse: false
+  });
+}
 
-  FS.writeFileSync(fullFileName, JSON.stringify(json, undefined, 2));
-}, {
-  filter: (fullFileName, fileName, stat) => {
-    if (stat.isDirectory())
-      return (!fileName.match(/^(react-ameliorate-react-native-shims)$/));
-
-    return (fileName === 'package.json');
-  }
-});
-
+copySupportFilesToPackages();
