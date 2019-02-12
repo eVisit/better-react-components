d36a46b3d216db09cadaeabec8eb0483479fcec4
Wyatt Greenway <th317erd@gmail.com>
2019-02-11 18:41:39 -0700

    Fix bad prop type

--- packages/react-ameliorate-native-shims/source/prop-types/color.js
+++ packages/react-ameliorate-native-shims/source/prop-types/color.js
@@ -1,5 +1,5 @@
 import PropTypes from '@react-ameliorate/prop-types';
 
-const ColorPropType = PropTypes.oneOf([PropTypes.number, PropTypes.string]);
+const ColorPropType = PropTypes.oneOfType([PropTypes.number, PropTypes.string]);
 
 export default ColorPropType;
