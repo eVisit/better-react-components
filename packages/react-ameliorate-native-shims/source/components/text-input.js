41777079d9135bd3f660bf2bdd5fa820ff317759
Aleksandr Telegin <99ff00@gmail.com>
2020-11-05 18:19:11 +0000

    fix: 🐞 scrolling issue with autofocused fields

--- packages/react-ameliorate-native-shims/source/components/text-input.js
+++ packages/react-ameliorate-native-shims/source/components/text-input.js
@@ -11,6 +11,107 @@ import {
 }                                 from '@react-ameliorate/utils';
 import TextInputPropTypes         from '../prop-types/text-input';
 
+
+(function () {
+  if (
+    typeof window === "undefined" ||
+    typeof document === "undefined" ||
+    typeof HTMLElement === "undefined"
+  ) {
+    return;
+  }
+
+  var supportsPreventScrollOption = false;
+  try {
+    var focusElem = document.createElement("div");
+    focusElem.addEventListener(
+      "focus",
+      function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+      },
+      true
+    );
+    focusElem.focus(
+      Object.defineProperty({}, "preventScroll", {
+        get: function () {
+          // Edge v18 gives a false positive for supporting inputs
+          if (
+            navigator &&
+            typeof navigator.userAgent !== 'undefined' &&
+            navigator.userAgent &&
+            navigator.userAgent.match(/Edge\/1[7-8]/)) {
+            return supportsPreventScrollOption = false
+          }
+
+          supportsPreventScrollOption = true;
+        }
+      })
+    );
+  } catch (e) { }
+
+  if (
+    HTMLElement.prototype.nativeFocus === undefined &&
+    !supportsPreventScrollOption
+  ) {
+    HTMLElement.prototype.nativeFocus = HTMLElement.prototype.focus;
+
+    var calcScrollableElements = function (element) {
+      var parent = element.parentNode;
+      var scrollableElements = [];
+      var rootScrollingElement =
+        document.scrollingElement || document.documentElement;
+
+      while (parent && parent !== rootScrollingElement) {
+        if (
+          parent.offsetHeight < parent.scrollHeight ||
+          parent.offsetWidth < parent.scrollWidth
+        ) {
+          scrollableElements.push([
+            parent,
+            parent.scrollTop,
+            parent.scrollLeft
+          ]);
+        }
+        parent = parent.parentNode;
+      }
+      parent = rootScrollingElement;
+      scrollableElements.push([parent, parent.scrollTop, parent.scrollLeft]);
+
+      return scrollableElements;
+    };
+
+    var restoreScrollPosition = function (scrollableElements) {
+      for (var i = 0; i < scrollableElements.length; i++) {
+        scrollableElements[i][0].scrollTop = scrollableElements[i][1];
+        scrollableElements[i][0].scrollLeft = scrollableElements[i][2];
+      }
+      scrollableElements = [];
+    };
+
+    var patchedFocus = function (args) {
+      if (args && args.preventScroll) {
+        var evScrollableElements = calcScrollableElements(this);
+        if (typeof setTimeout === 'function') {
+          var thisElem = this;
+          setTimeout(function () {
+            thisElem.nativeFocus();
+            restoreScrollPosition(evScrollableElements);
+          }, 0);
+        } else {
+          this.nativeFocus();
+          restoreScrollPosition(evScrollableElements);
+        }
+      }
+      else {
+        this.nativeFocus();
+      }
+    };
+
+    HTMLElement.prototype.focus = patchedFocus;
+  }
+})();
+
 class TextInputShim extends React.Component {
   constructor(props, ...args) {
     super(props, ...args);
@@ -42,7 +143,7 @@ class TextInputShim extends React.Component {
 
   componentDidMount = () => {
     if (this.props.autoFocus === true && this._inputRef)
-      this._inputRef.focus();
+      this._inputRef.focus({ preventScroll: true });
 
     if (this.props.multiline && this._inputRef) {
       this._inputRef.addEventListener('input', this.autoExpand);
@@ -223,7 +324,7 @@ class TextInputShim extends React.Component {
     this._inputRef = elem;
 
     if (elem && this.props.autoFocus === true)
-      elem.focus();
+      elem.focus({ preventScroll: true });
 
     assignRef(this.props.inputRef, elem);
   }
@@ -236,7 +337,7 @@ class TextInputShim extends React.Component {
     if (!this._inputRef)
       return;
 
-    this._inputRef.focus();
+    this._inputRef.focus({ preventScroll: true });
   }
 
   blur = () => {
